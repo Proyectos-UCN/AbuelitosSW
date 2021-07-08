40baf44f9c14d7ba12f0f54de15e281a8728c0ef
NicolasGarciaS <ngarcia1621@gmail.com>
2021-07-07 20:40:11 -0400

    call
    llamada lista.

---
+++ Assets/callMenu/NewBehaviourScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject panel;
+      bool state;
+  
+      public void SwitchShowHide()
+      {
+          state = !state; 
+          panel.gameObject.SetActive (state);
+      }
+}
